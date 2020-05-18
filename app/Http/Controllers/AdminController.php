c4071263bbd0862951a9f7fdbd79b006e61af091
Raul Gonzalez <bragr77@gmail.com>
2020-05-18 16:39:37 -0400

    Reportes de entidades en la administración

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,10 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Hero;
+use App\Item;
+use App\Enemy;
 
 class AdminController extends Controller
 {
     public function index(){
-        return view('admin.index');
+
+        $herocounter    = Hero::count();
+        $itemcounter    = Item::count();
+        $enemycounter   = Enemy::count();
+
+        $report = [
+
+            ['name' => "Heroes", 'counter' => $herocounter, 'route' => 'heroes.index', 'class'=>"btn-outline-primary"],
+            ['name' => "Items", 'counter' => $itemcounter, 'route' => 'item.index', 'class'=>"btn-outline-success"],
+            ['name' => "Enemigos", 'counter' => $enemycounter, 'route' => 'enemy.index', 'class'=>"btn-outline-danger"],
+
+        ];
+
+        return view('admin.index', ['report' => $report]);
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,5 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>ADMINISTRADOR</h1>
+    <div class="row">
+        <h1>ADMINISTRADOR DE HERO</h1>
+    </div>
+
+    <div class="row">
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">Entidad</th>
+                    <th scope="col">Cantidad de Elementos</th>
+                    <th scope="col">Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($report as $item)
+
+                <tr>
+                    <td>{{ $item["name"] }}</td>
+                    <td>{{ $item["counter"] }}</td>
+                    <td>
+                        <a href="{{ route($item["route"]) }}" class="btn {{ $item["class"] }} btn-block">Ir a {{ $item["name"] }}</a>
+                    </td>
+                </tr>
+
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
