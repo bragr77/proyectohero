ed8522943f718ebd388787b4bcdcab8430ad6f31
Raul Gonzalez <bragr77@gmail.com>
2020-06-01 09:04:01 -0400

    Sistema de batalla Manual, y agregamos el factor suerte que trae el heroe por base de datos

--- app/Http/Controllers/BSController.php
+++ app/Http/Controllers/BSController.php
@@ -11,6 +11,8 @@ class BSController extends Controller
 {
     public function index(){
 
+        //dd($this->runManualBattle(1,2));
+
         return view('admin.bs.index', $this->runAutoBattle(1,2));
 
     }
@@ -26,7 +28,7 @@ class BSController extends Controller
 
             $luck = random_int(0, 100);
 
-            if ($luck >= 50) {
+            if ($luck >= $hero->luck) {
 
                 $hp = $enemy->def - $hero->atq;
 
@@ -86,5 +88,63 @@ class BSController extends Controller
             "heroName" => $hero->name,
             "enemyName" => $enemy->name
         ];
+    }
+
+    public function runManualBattle($heroId, $enemyId){
+
+        $hero   = Hero::find($heroId)->first();
+        $enemy  = Enemy::find($enemyId)->first();
+
+        $luck = random_int(0, 100);
+
+        if ($luck > $hero->luck) {
+            $hp = $enemy->def - $hero->atq;
+
+            if ($hp < 0) {
+                $enemy->hp -= $hp * -1;
+            }
+
+            if ($enemy->hp > 0) {
+                return [
+                    "winner" => "hero",
+                    "text" => $hero->name . " hizo un daño de " . $hero->atq . " a " . $enemy->name
+                ];
+            }else {
+                return [
+                    "winner" => "hero",
+                    "text" => $hero->name . " acabo con la vida de " . $enemy->name . " y gano " . $enemy->xp . " de experiencia"
+                ];
+
+                $hero->xp = $hero->xp + $enemy->xp;
+
+                if ($hero->xp >= $hero->level->xp) {
+                    $hero->xp = 0;
+                    $hero->level_id += 1;
+                }
+
+                $hero->save();
+            }
+
+        } else {
+
+            $hp = $hero->def -$enemy->atq;
+
+            if ($hp < 0) {
+                $hero->hp -= $hp * -1;
+            }
+
+            if ($hero->hp > 0) {
+                return [
+                    "winner" => "enemy",
+                    "text" => $hero->name . " recibio un daño de " . $enemy->atq . " por " . $enemy->name
+                ];
+            } else {
+                return [
+                    "winner" => "enemy",
+                    "text" => $hero->name . " fue asesinado por " . $enemy->name
+                ];
+            }
+        }
+
     }
 }
